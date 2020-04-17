a009224a3656ce47bee0c61e5fdaa6d650bd188b
Song <quanpengsong@gmail.com>
2020-04-17 20:54:05 +0800

    :new: now you can set the port by config the env parameter

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"lan-share/model"
+	"os"
 	"strconv"
 )
 
 var (
-	port = flag.Int("port", 3000, "specify the server listening port.")
+	port  = flag.Int("port", 3000, "specify the server listening port.")
 	Token = flag.String("token", "token", "specify the private token.")
 )
 
@@ -26,5 +27,9 @@ func main() {
 	server.LoadHTMLGlob("static/template.gohtml")
 	SetIndexRouter(server)
 	SetApiRouter(server)
-	_ = server.Run(":"+strconv.Itoa(*port))
+	var realPort = os.Getenv("PORT")
+	if realPort == "" {
+		realPort = strconv.Itoa(*port)
+	}
+	_ = server.Run(":" + realPort)
 }
