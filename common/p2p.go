9875227b7287dd62636bb14f25ed465ae3633cfe
JustSong <songquanpeng@foxmail.com>
2022-09-18 12:28:14 +0800

    :construction: update P2P server protocol

--- common/p2p.go
+++ common/p2p.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net"
 	"strconv"
 	"time"
@@ -23,16 +24,49 @@ type SendPocket struct {
 
 func p2pLog(s string) {
 	t := time.Now()
-	fmt.Fprintf(gin.DefaultWriter, "[P2P] %v | %s \n", t.Format("2006/01/02 - 15:04:05"), s)
+	_, _ = fmt.Fprintf(gin.DefaultWriter, "[P2P] %v | %s \n", t.Format("2006/01/02 - 15:04:05"), s)
 }
 
-func sendPockets(server *net.UDPConn, senderAddr net.UDPAddr, receiverAddr net.UDPAddr) {
-	_, err := server.WriteToUDP([]byte(senderAddr.String()), &receiverAddr)
+// Server Response Protocol:
+// The first byte is the status byte, is an uint8 number, 0 means okay, 1 means error.
+// The remaining bytes is the data to be sent.
+
+var OkayByte uint8 = 0
+var ErrorByte uint8 = 1
+
+func sendIdAssignmentPocket(server *net.UDPConn, receiverAddr *net.UDPAddr, id uint64) {
+	buffer := make([]byte, 9)
+	buffer[0] = OkayByte
+	binary.LittleEndian.PutUint64(buffer[1:], id)
+	_, err := server.WriteToUDP(buffer, receiverAddr)
 	if err != nil {
 		p2pLog("error during send to " + receiverAddr.String())
 		return
 	}
-	_, err = server.WriteToUDP([]byte(receiverAddr.String()), &senderAddr)
+}
+
+func sendIdNotFoundPocket(server *net.UDPConn, receiverAddr *net.UDPAddr) {
+	buffer := make([]byte, 1)
+	buffer[0] = ErrorByte
+	_, err := server.WriteToUDP(buffer, receiverAddr)
+	if err != nil {
+		p2pLog("error during send to " + receiverAddr.String())
+		return
+	}
+}
+
+func sendConnPockets(server *net.UDPConn, senderAddr *net.UDPAddr, receiverAddr *net.UDPAddr) {
+	buffer := make([]byte, 1)
+	buffer[0] = OkayByte
+	buffer = append(buffer, []byte(senderAddr.String())...)
+	_, err := server.WriteToUDP(buffer, receiverAddr)
+	if err != nil {
+		p2pLog("error during send to " + receiverAddr.String())
+		return
+	}
+	buffer = buffer[:1]
+	buffer = append(buffer, []byte(receiverAddr.String())...)
+	_, err = server.WriteToUDP(buffer, senderAddr)
 	if err != nil {
 		p2pLog("error during send to " + senderAddr.String())
 		return
@@ -53,18 +87,34 @@ func StartP2PServer() {
 			p2pLog("error during read from udp: " + err.Error())
 			continue
 		}
-		id := binary.BigEndian.Uint64(buffer[:8])
-		if addr2, ok := id2addr[id]; ok {
-			p2pLog("send pocket to " + addr.String() + " & " + addr2.String())
-			delete(id2addr, id)
-			go sendPockets(server, addr2, *addr)
-		} else {
+		id := binary.LittleEndian.Uint64(buffer[:8])
+		if id == 0 {
+			// Sender request a unique id
 			if len(id2addr) > maxMapSize {
 				p2pLog("too many items in id2addr, reset it")
 				id2addr = make(map[uint64]net.UDPAddr)
 			}
-			p2pLog("register id " + strconv.FormatUint(id, 10) + " with " + addr.String())
+			for {
+				id = rand.Uint64()
+				if _, ok := id2addr[id]; ok {
+					continue
+				} else {
+					break
+				}
+			}
+			p2pLog("assign id " + strconv.FormatUint(id, 10) + " to " + addr.String())
 			id2addr[id] = *addr
+			go sendIdAssignmentPocket(server, addr, id)
+		} else {
+			// Receiver register with id
+			p2pLog("register id " + strconv.FormatUint(id, 10) + " with " + addr.String())
+			if addr2, ok := id2addr[id]; ok {
+				p2pLog("send connection pockets to " + addr.String() + " & " + addr2.String())
+				delete(id2addr, id)
+				go sendConnPockets(server, &addr2, addr)
+			} else {
+				go sendIdNotFoundPocket(server, addr)
+			}
 		}
 	}
 }
