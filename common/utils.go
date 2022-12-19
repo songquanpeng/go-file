2942a21c2cc508f3eaef1229f1940e5f875b88bb
JustSong <quanpengsong@gmail.com>
2022-12-19 16:15:49 +0800

    :tada: mute error when unable to open browser

--- common/utils.go
+++ common/utils.go
@@ -12,18 +12,13 @@ import (
 )
 
 func OpenBrowser(url string) {
-	var err error
-
 	switch runtime.GOOS {
 	case "linux":
-		err = exec.Command("xdg-open", url).Start()
+		_ = exec.Command("xdg-open", url).Start()
 	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+		_ = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
 	case "darwin":
-		err = exec.Command("open", url).Start()
-	}
-	if err != nil {
-		log.Println(err)
+		_ = exec.Command("open", url).Start()
 	}
 }
 
